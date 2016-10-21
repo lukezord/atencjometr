5c796c84bc1a17438fa5a90159ea1806c18debc4
lukezord <stfu.noob.kk@gmail.com>
2016-10-21 16:59:19 +0200

    sql data management v0.1
    i dont know what im doing XD

--- atencjometr.py
+++ atencjometr.py
@@ -6,8 +6,6 @@ TODO:
 - user i token pobierany automatycznie?
 - dodac ranking
 - monitorowanie sub4sub
-- zrzut do db
-- sprawdzic czy tabele istnieja czy nie 
 - porownac wyniki z tymi w bazie
 """
 
@@ -20,24 +18,27 @@ api.authenticate(username, accountkey)
 profile = api.get_profile(username)
 
 
-
-
 conn = sqlite3.connect('atencja.db')
 c = conn.cursor()
-#tworzymy baze
+
 c.execute('''CREATE TABLE IF NOT EXISTS podsumowanie
 (obserwujacy real, obserwujesz real)''')
 c.execute('''CREATE TABLE IF NOT EXISTS obserwuja
-(followers text)''')
+(followers text UNIQUE)''')
 c.execute('''CREATE TABLE IF NOT EXISTS obserwuje
-(followed text)''')
+(followed text UNIQUE)''')
 c.execute ("INSERT INTO podsumowanie VALUES (?, ?)", [profile["followers"], profile["following"]]) #ilosc subow
 for i in range(1, 16):
     for followers_list in api.get_profile_followers(username, page=i):
-        c.execute ("INSERT INTO obserwuja VALUES (?)", [followers_list["login"]]) #obserwuja mnie
+        c.execute ("INSERT OR IGNORE INTO obserwuja VALUES (?)", [followers_list["login"]]) #obserwuja mnie
 for i in range(1,16):
     for followed_list in api.get_profile_followed(username, page=i):
-        c.execute ("INSERT INTO obserwuje VALUES (?)", [followed_list["login"]]) #ja obserwuje
+        c.execute ("INSERT OR IGNORE INTO obserwuje VALUES (?)", [followed_list["login"]]) #ja obserwuje
+
+for i in range(1,16):
+    for followed_list in api.get_profile_followed(username, page=i):
+        for row in c.execute ("SELECT * FROM obserwuje WHERE followed NOT IN (?)", [followed_list["login"]])
+        print row
 
 conn.commit()
 conn.close()
